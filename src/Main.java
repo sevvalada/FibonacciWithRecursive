7706028361b2922889bfcefd6c191a515114be6e
Sevval Havva Ada <adasevval36@gmail.com>
2025-11-02 17:15:40 +0300

    Fibonacci Recursive ile Yapildi

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Main {
+    static int fib(int n) {
+        if (n == 0 ){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+
+public static void main(String[] args) {
+    System.out.println(fib(6));
+}
+}
